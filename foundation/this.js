b0313a9acb629895ed2415933ea7850d93348638
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-06-05 13:32:27 +0200

    lesson 46

---
+++ foundation/this.js
@@ -0,0 +1,37 @@
+// this is the object that the function is a property of
+
+/**
+ * 1: gives methods access to their object
+ * 2: execute same code for multiple objects
+ */
+
+const obj = {
+  name: "Billy",
+  sing() {
+    return `lalala ${this.name}`  // kind of equivalent to run `lalala ${obj.name}`
+  },
+  singAgain() {
+    return this.sing() + "!"
+  }
+}
+
+function importantPerson() {
+  console.log(this.name)
+}
+
+const name = "Sunny"
+// will reference to the correct one due to using this keyword --> references the parent obj
+const obj1 = {
+  name: "Cassy",
+  importantPerson,
+}
+const obj2= {
+  name: "Jacob",
+  importantPerson,
+}
+
+console.log(obj.sing())
+console.log(obj.singAgain())
+
+obj1.importantPerson()
+obj2.importantPerson()
