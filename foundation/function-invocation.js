94f37b23220c99bd53f23287b8f99a4b050cb28b
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-06-05 10:49:27 +0200

    lesson 36

---
+++ foundation/function-invocation.js
@@ -0,0 +1,19 @@
+// function expression
+var canada = () => {
+  console.log("cold")
+}
+
+// Function declaration
+function india() {
+  console.log("warm")
+}
+
+// function invocation/calling/execution
+canada()
+india()
+
+function marry(person1, person2) {
+  console.log(arguments)
+}
+
+marry("sldfj", "fskj")
