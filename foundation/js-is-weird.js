04b7e4d48fe70f3ac8089a772666f19f1b605eef
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-06-05 11:23:45 +0200

    lesson 41

---
+++ foundation/js-is-weird.js
@@ -0,0 +1,7 @@
+var heyhey = function doodle() {
+  // doodle() --> we should only name a function here if we want recursion
+  return "heyhey"
+}
+
+heyhey()
+// doodle() --> does not exist in global scope, only in its own scope.
