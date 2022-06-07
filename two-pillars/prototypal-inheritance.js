b41016c1a7e9151c545a5db7454e9612b9f02dfb
Alexander Törnvall <alextorn.python@gmail.com>
2022-06-07 16:17:16 +0200

    lesson 83

---
+++ two-pillars/prototypal-inheritance.js
@@ -0,0 +1,19 @@
+let dragon = {
+  name: "Tanya",
+  fire: true,
+  fight() {
+    return 5
+  },
+  sing() {
+    if (this.fire) {
+      return `I am ${this.name}, the breather of fire.`
+    }
+  },
+}
+
+let lizard = {
+  name: "Kiki",
+  fight() {
+    return 1
+  }
+}
