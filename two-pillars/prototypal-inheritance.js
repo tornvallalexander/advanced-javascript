72b29d04b6ea8254d8e5d2f48a999b6226aae342
Alexander Törnvall <alextorn.python@gmail.com>
2022-06-07 19:36:17 +0200

    lesson 84

--- two-pillars/prototypal-inheritance.js
+++ two-pillars/prototypal-inheritance.js
@@ -22,7 +22,7 @@ let lizard = {
 // const singLizard = dragon.sing.bind(lizard)
 
 // creating a chain --> lizard all the way up to dragon
-lizard.__proto__ = dragon
+lizard.__proto__ = dragon  // __proto__ is not something we should use, but demonstrates the basic idea
 
 // looks up in the prototype chain
 // lizard.sing()
@@ -30,7 +30,14 @@ lizard.__proto__ = dragon
 dragon.isPrototypeOf(lizard)  // is dragon a prototype of lizard --> does lizard inherit from dragon?
 for (let prop in lizard) {
   console.log(prop)
+  // shows which properties are inherited, checks and goes up the prototype chain
   if (lizard.hasOwnProperty(prop)) {
     console.log("own: ", prop)
   }
 }
+
+/**
+ * Prototypal inheritance is very performant, uses less memory --> keeping code DRY
+ */
+const testObj = {}
+console.log(testObj.__proto__.__proto__)  // returns null because this is as far up the chain that we can go
