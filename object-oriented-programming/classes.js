b605c1e9c55bd867115b403851bedf7e17ee78f8
Alexander Törnvall <alextorn.python@gmail.com>
2022-06-08 20:36:11 +0200

    lesson 101

---
+++ object-oriented-programming/classes.js
@@ -0,0 +1,20 @@
+// ES6 classes
+
+// class is actually just syntactic sugar in JavaScript, it still uses the new keyword under the hood
+class Elf {
+  // runs everytime we instantiate
+  constructor(name, weapon) {
+    this.name = name
+    this.weapon = weapon
+  }
+  // attack is shared by all instances of the class. The reason we don't put it in the constructor is to avoid it being
+  // instantiated every time we create a new instance of the class. Way better memory-wise.
+  attack() {
+    return `attack with ${this.weapon}`
+  }
+}
+// way cleaner than what we did before... one location for everything
+
+// peter is an instance --> whenever we call a class we create an instance
+const peter = new Elf("Peter", "stones")
+console.log(peter.attack())
