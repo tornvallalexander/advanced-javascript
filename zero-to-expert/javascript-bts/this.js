1cd98c342c0becb74ef19a567fec4d6c938a45c4
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-06-25 16:30:48 +0200

    arrow function vs normal function

--- zero-to-expert/javascript-bts/this.js
+++ zero-to-expert/javascript-bts/this.js
@@ -23,12 +23,33 @@ const calcAgeArrow = (birthYear) => {
 calcAgeArrow(1991);
 
 const jonas = {
+  firstName: "Jonas",
   year: 1991,
   calcAge: function() {
     console.log(this);  // points to the object itself (the jonas object) --> jonas is the owner of the method
+    // the way we can solve this often like this:
+    // this is the older solution
+    const self = this;
+    const isMillennial = function() {
+      console.log(self.year >= 1981 && self.year <= 1996); // as the rule states, the this keyword is undefined inside a function
+    }
+    isMillennial()
+
+    const isMillennial2 = function() {
+      console.log(self.year >= 1981 && self.year <= 1996); // as the rule states, the this keyword is undefined inside a function
+    }.bind(this)
+    isMillennial2()
+
+    // New solution:
+    const isMillennial3 = () => {
+      console.log(this.year >= 1981 && this.year <= 1996); // this points to the jonas object
+    }
+
     return 2037 - this.year;
-  }
+  },
+  greet: () => console.log(`Hello ${this.firstName}`),
 }
+jonas.greet(); // Hello undefined --> will use parent scope, which is the global scope (object is only a literal and not parent scope)
 jonas.calcAge();
 
 
