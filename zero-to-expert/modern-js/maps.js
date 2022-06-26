6b4fb7694c2fe3c078a26c457b3c4ba2992e3922
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-06-26 18:06:41 +0200

    course challenge maps, sets, arrays

--- zero-to-expert/modern-js/maps.js
+++ zero-to-expert/modern-js/maps.js
@@ -20,13 +20,15 @@ const currentTime = new Date().getHours();
 console.log(currentTime);
 console.log(opened.get(rest.get("open") <= currentTime && currentTime < rest.get("close")));
 
-// this is also how the DOM works, and can work
-const DOM = new Map();
-DOM.set("pizza", document.getElementById("pizza"));
-DOM.get("pizza").innerHTML = "Pizza Hut";
+if (typeof document !== "undefined") {
+  // this is also how the DOM works, and can work
+  const DOM = new Map();
+  DOM.set("pizza", document?.getElementById?.("pizza"));
+  DOM.get("pizza").innerHTML = "Pizza Hut";
 
-// also works the other way around
-DOM.set(document.getElementById("pizza"), "pizza");
+  // also works the other way around
+  DOM.set(document?.getElementById?.("pizza"), "pizza");
+}
 
 
 // working with maps
@@ -44,4 +46,30 @@ const quiz = new Map([
 // function to convert an object to a map
 const objectToMap = (obj) => {
   return new Map(Object.entries(obj))
+}
+
+const gameEvents = new Map([
+  [17, "Goal"],
+  [21, "Assist"],
+  [34, "Substitution"],
+  [49, "Goal"],
+  [64, "Yellow Card"],
+  [78, "Red Card"],
+  [92, "Goal"],
+])
+
+const events = [...new Set([...gameEvents])]; // [17, 21, 34, 49, 64, 78, 92] - unique values and turned into an array
+
+// remove yellow card from gameEvents
+gameEvents.delete(64);
+
+console.log(`An event happened roughly every ${(90 / events.length).toFixed(0)} minutes`);
+
+const halfs = new Map([
+  [true, "First Half"],
+  [false, "Second Half"],
+]);
+
+for (const event of events) {
+  console.log(`${event} - ${halfs.get(event[0] <= 45)}`);
 }
