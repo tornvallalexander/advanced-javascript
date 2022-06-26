794dbe1365c5530bf4023367b24ef292f7421a39
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-06-26 14:14:15 +0200

    iterating over objects and arrays

---
+++ zero-to-expert/modern-js/iterating.js
@@ -0,0 +1,39 @@
+const arr = [1, 2, 3, 4, 5];
+
+// ES6 of operator
+for (const item of arr) {
+  // gets the value
+  // console.log(item);
+}
+
+for (const [i, el] of arr.entries()) {
+  // gets the entire object
+  // console.log(el, i)
+}
+
+// Object literals
+const obj = {
+  mon: "Monday",
+  tue: "Tuesday",
+  wed: "Wednesday",
+  thu: "Thursday",
+  fri: "Friday",
+  sat: "Saturday",
+  sun: "Sunday",
+}
+
+// important difference: arr.entries() does not need arg while obj.entries() does
+// Object.keys(obj) turns the objects keys into an array
+for (const key of Object.keys(obj)) {
+  // console.log(key)
+}
+
+// Object.values(obj) turns the objects values into an array
+for (const val of Object.values(obj)) {
+  // console.log(val)
+}
+
+// pretty much the same way as with arrays
+for (const [key, value] of Object.entries(obj)) {
+  // console.log(key, value)
+}
