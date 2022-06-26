dac78cd5271f36f6da7741ffaea631718bc5f041
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-06-26 21:32:01 +0200

    basic string operations #2

--- zero-to-expert/modern-js/string-operations.js
+++ zero-to-expert/modern-js/string-operations.js
@@ -15,3 +15,20 @@ console.log(username.replace(regex, ""))
 
 // we can see this in action by doing the following:
 console.log(new String("Hello there")) // here we also get all the methods on the String object
+
+// function to capitalize string
+function capitalize(str) {
+  return str.toLowerCase().charAt(0).toUpperCase() + str.slice(1)
+}
+
+// function to
+// 1. lowercase string
+// 2. remove all whitespaces
+// 3. remove all non-alphanumeric characters
+
+function cleanString(str) {
+  return str.toLowerCase().replace(/\s/g, "").replace(/\W/g, "")
+}
+
+const test = "  Hello@alexAnder.io  \n."
+console.log(cleanString(test))
