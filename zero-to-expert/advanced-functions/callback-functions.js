b4dbae812fe8acd04deab5dfefb5f9fe1de427fc
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-06-28 20:20:09 +0200

    theory callback functions #2

--- zero-to-expert/advanced-functions/callback-functions.js
+++ zero-to-expert/advanced-functions/callback-functions.js
@@ -7,9 +7,15 @@ const upperFirstWord = function(str) {
   return str.replace(str[0], str[0].toUpperCase())
 }
 
-// higher order function
+// higher order function - specifically a callback function
+// JS uses callback functions all the time
 const transformer = function(str, fn) {
+  console.log(`I was called by ${fn.name}`)
   return fn(str)
 }
 
 transformer("JavaScript is a fun language", oneWord)
+
+// callback functions allow us to create abstractions
+// abstraction is when we hide the details of a function because
+// we don't really need to know about it
