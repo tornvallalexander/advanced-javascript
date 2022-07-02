892bf365c5632811a6bbaacab41045463588b492
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-07-02 15:22:09 +0200

    reduce method

--- zero-to-expert/arrays/data-transformations.js
+++ zero-to-expert/arrays/data-transformations.js
@@ -29,3 +29,29 @@ console.log(arr.map((x) => {
 // using map vs forEach
 // forEach is used to iterate over an array with side effects
 // map is used to iterate over an array and return a new array
+
+// reduce
+// reduce() is used to reduce an array to a single value
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const balance = movements.reduce((acc, curr, i, arr) => {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + curr
+}, 100); // 0 is the initial value, 0 by default
+console.log(balance);
+
+// reduce simply avoid having to create extra variables to store the accumulator:
+let balance2 = 100;
+for (const movement of movements) {
+  console.log(`Iteration ${movement}: ${balance2}`);
+  balance2 += movement;
+}
+
+// reduce on strings
+const str = "Alexander";
+const addSpaceBetweenChar = (s) => {
+  return s
+    .split("")
+    .reduce((acc, curr) => acc += curr + " ", "")
+}
+console.log(addSpaceBetweenChar(str));
