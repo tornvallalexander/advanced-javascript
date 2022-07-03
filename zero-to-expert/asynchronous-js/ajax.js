293ef2091a4bed56ab65841dc27de054a3f33f44
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-07-03 16:01:33 +0200

    feat: handling errors directly in then callback

--- zero-to-expert/asynchronous-js/ajax.js
+++ zero-to-expert/asynchronous-js/ajax.js
@@ -2,9 +2,26 @@
 
 // old way of making ajax requests
 const country = "sweden"
+/*
 const request = new XMLHttpRequest();
 request.open("GET", `https://restcountries.com/rest/v2/${country}`);
 request.send();
 request.addEventListener("load", function () {
   console.log(this.responseText);
-})
+})*/
+
+// new way of doing it
+
+function getCountryData(country) {
+  const response = fetch(`https://restcountries.com/v2/name/${country}`);
+  response.then(
+    (res) => {
+      console.log(res);
+    },
+    (err) => {
+      console.log(err);
+    }
+  )
+}
+
+getCountryData("sweden");
