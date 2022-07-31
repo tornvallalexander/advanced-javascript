8a788bc7a08fa08f2dad9c425d618a0666665abd
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-07-31 13:34:22 +0200

    feat(docs): describing how modules works

---
+++ zero-to-expert/modern-javascript-development/modules.js
@@ -0,0 +1,9 @@
+/**
+ * Modules are always imported synchronously. This means that the module is loaded and the code is executed
+ * before the rest of the code in the file is executed.
+ * This is only possible due to top-level 'static' imports, this makes the imports known before execution.
+ * Although we tend to think of synchronous as bad, in this case, it is relly the simplest way to do it. It is what
+ * makes dead code elimination possible.
+ *
+ * Interestingly, the code exported and imported are references, not copies. This is quite unique to ES6 modules.
+ */
