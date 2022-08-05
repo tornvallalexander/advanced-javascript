37b4d10398c4042227a158587b0bb464e4f64cd5
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-08-05 12:57:26 +0200

    feat(memo): attempt for memoization on compose function

--- ztm/functional-programming/compose.mjs
+++ ztm/functional-programming/compose.mjs
@@ -1,10 +1,27 @@
 // term: arity
 // arity is the number of arguments that a function takes, a common term in functional programming
 // the few number of parameters, the easier it is to use that program
+let search = "voluptatem"
 
 const compose = (...fns) => (val) => fns.reduce((acc, curr) => curr(acc), val)
 const pipe = (...fns) => (val) => fns.reverse().reduce((acc, curr) => curr(acc), val) // same as compose but in reverse,
-// right to left
+// right to left --> seems like there is some contradiction, I now believe it is the other way around.
+
+// adding memoization
+const memoize = (fn) => {
+  const cache = new Map();
+  return (...args) => {
+    if (cache.has(args)) {
+      return cache.get(args);
+    }
+    const result = fn(...args);
+    cache.set(args, result);
+    console.log(cache.get(args));
+    return result;
+  }
+}
+
+const memoizedCompose = memoize(compose);
 
 const limit = (n) => (arr) => arr?.slice?.(0, n);
 
@@ -22,8 +39,8 @@ const max = (maxNum, element) => {
   }
 }
 
-const filterPosts = compose(
-  includes("voluptatem", "body"),
+const filterPosts = memoizedCompose(
+  includes(search, "body"),
   max(4, "title"),
   limit(10),
 );
