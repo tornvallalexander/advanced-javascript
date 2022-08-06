083b879d689053b2940fc0484ce29461de84b9a9
Alexander Törnvall <alexander.tornvall@icloud.com>
2022-08-06 09:14:56 +0200

    feat(amazon): combo of inheritance and composition

--- ztm/functional-programming/amazon-inheritance.js
+++ ztm/functional-programming/amazon-inheritance.js
@@ -1,5 +1,19 @@
 // amazon store with inheritance
 
+// inheritance in contrast to composition, defines "what it is". With composition, we define "what it has", or
+// what it does to data.
+
+// inheritance tends to lead to a lot of dependencies, where if you change something in a parent class,
+// this instantly has rippling effects in all the child classes:
+/*
+--> Tight coupling
+--> Fragile base class problem
+--> Hierarchy problem
+
+As a code base grows, the number of dependencies increases. This makes it very hard to change something in a parent
+without having major changes in all the child classes. There will be edge cases where we need to change something,
+but we don't want to change it in all the child classes. This is where composition makes much more sense than inheritance.
+ */
 class User {
   constructor(name, active = true, cart = [], purchases = []) {
     this.name = name;
@@ -40,3 +54,41 @@ console.log(kim);
 
 // honestly, inheritance is working pretty nicely here.
 
+// composition
+const pipe = (...fns) => (args) => fns.reduce((acc, curr) => curr(acc), args);
+function Customer(name, active = true, cart = [], purchases = []) {
+  const user = {
+    name,
+    active,
+    cart,
+    purchases,
+  }
+
+  // this is extremely nice when comparing to inheritance. Gives us way more flexibility and I love the simplicity.
+  return pipe(
+    getHistory,
+    getUsername,
+  )(user);
+  // return getHistory(user);
+}
+
+function getHistory(user) {
+  return {
+     history() {
+       return user.purchases
+     },
+    ...user,
+  };
+}
+
+function getUsername(user) {
+  return {
+    username() {
+      return user.name
+    },
+    ...user,
+  }
+}
+
+const alex = Customer('Alex');
+console.log(alex);
